7f165276ad462738cd093a1ee7ee8ac48258794b
allencho <allen.my.cho@gmail.com>
2018-11-23 11:08:29 +1100

    Error fix

--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace GithubApi
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=GithubApi}/{id?}");
             });
         }
     }
